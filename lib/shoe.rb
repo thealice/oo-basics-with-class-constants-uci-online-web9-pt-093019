58650828a9d15ce7597e9799790d923da1e06108
Alice Freda <alicefreda@gmail.com>
2019-10-21 05:21:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if BRANDS.none?("#{brand}") puts BRANDS << brand
+    BRANDS << brand if BRANDS.none?("#{brand}")
   end
 
   def cobble
